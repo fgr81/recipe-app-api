35368eb63d69dc587fdf906b059c90fdad5716f8
fgr81 <fgr81@hotmail.it>
2023-01-17 18:25:56 +0100

    finito 15

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
     'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
